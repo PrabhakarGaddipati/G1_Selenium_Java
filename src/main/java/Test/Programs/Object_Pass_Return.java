6b08ee8913e7ac4c95320d6913a9dd44b216941f
prabhakargaddipati <prabhakargaddipati@gmail.com>
2024-09-15 20:21:18 +0530

    15/09/2024

---
+++ src/main/java/Test/Programs/Object_Pass_Return.java
@@ -0,0 +1,39 @@
+package Test.Programs;
+
+import java.util.Scanner;
+public class Object_Pass_Return 
+{
+    int length, breadth, area;
+    Object_Pass_Return area1(Object_Pass_Return object1)
+    {
+        //object1 = new Object_Pass_Return();
+        object1.length = this.length;
+        object1.breadth = this.breadth;
+        object1.area = object1.length * object1.breadth;
+        return object1;
+    }
+    Object_Pass_Return area2(Object_Pass_Return object2)
+    {
+        object2 = new Object_Pass_Return();
+        object2.length = this.length + 5;
+        object2.breadth = this.breadth + 6;
+        object2.area = object2.length * object2.breadth;
+        return object2;
+    }
+    public static void main(String[] args) 
+    {
+         Object_Pass_Return obj = new Object_Pass_Return();
+        
+         Scanner s = new Scanner(System.in);
+         System.out.print("Enter length:");
+         obj.length = s.nextInt();
+         
+         System.out.print("Enter breadth:");
+         obj.breadth = s.nextInt(); 
+         
+         Object_Pass_Return a = obj.area1(obj);
+         Object_Pass_Return b = obj.area2(obj);
+         System.out.println("Area:"+a.area);
+         System.out.println("Area:"+b.area);
+    }
+}
---
+++ src/main/java/Test/Programs/P1_String_Concatenate.java
@@ -0,0 +1,31 @@
+package Test.Programs;
+
+import java.util.Scanner;
+
+public class P1_String_Concatenate
+{
+	public static void main(String[] args) 
+	{
+		String a, b, c;
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter first string:");
+		a = s.nextLine();
+
+		System.out.print("Enter second string:");
+		b = s.nextLine();
+
+		P1_String_Concatenate obj = new  P1_String_Concatenate();
+		c = obj.concat(a, b);
+		System.out.println("New String:"+c);
+	}
+		String concat(String x, String y)
+		{
+			String z;
+			z = x + " " + y;
+			return z;
+		}
+}
+
+//====[Output]===========================================
+//
+
